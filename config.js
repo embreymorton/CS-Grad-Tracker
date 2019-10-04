2470a109e61ac685931c956c4a67862ca135d906
Takoda <takoda.ren@gmail.com>
2019-10-04 11:05:06 -0400

    Switched to using .env file, added routes for testing/development only to change the user.

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    production: 'mongodb://localhost/cs_grad_data',
-    development: 'mongodb://localhost/cs_grad_data-dev',
-    testing: 'mongodb://localhost/cs_grad_data-test',
-    port: (process.env.MONGODB_PASSWORD) ? 8080 : 8080,
-    host: (process.env.MONGODB_PASSWORD) ? '0.0.0.0' : '127.0.0.1'
-}
-
-module.exports = config;
